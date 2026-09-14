80143a079a5878e10b6aa1369ed1fbd92b139dc5
Kauã Santana <kauasilvarino@gmail.com>
2026-09-14 02:49:51 -0300

    atualização nos comentários

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -4,8 +4,8 @@ print("Acabei de pensar em um número de 0 a 10.")
 jogador = " "
 somatentativas = 0
 while jogador != computador: #enquanto o número for diferente 
-    jogador = int(input("Qual seu palpite? "))
-    somatentativas += 1
+    jogador = int(input("Qual seu palpite? ")) #input do palpite
+    somatentativas += 1 #a cada ciclo do laço, soma-se uma tentativa a mais
     if jogador < computador: #se o num aleatorio for maior,
         print("Mais... Tente mais uma vez.")
     elif jogador > computador: #se o num aleatorio for menor,
